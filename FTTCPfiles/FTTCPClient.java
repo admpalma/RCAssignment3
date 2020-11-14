290fdab1909341c29d9914662a2d52b69979106b
André Palma <andremarquespalma@gmail.com>
2020-11-14 15:23:47 +0000

    Add Assignment Files

---
+++ FTTCPfiles/FTTCPClient.java
@@ -0,0 +1,24 @@
+/**
+ * FTTCPClient - File TCP Transfer Client
+ * A client that can send files to a FTTCPServer
+ * Initial material for background to Assignment 3
+ */
+
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class FTTCPClient {
+
+    static final int BLOCKSIZE = 1024; // you can change if you want
+	
+
+	public static void main(String[] args) throws Exception {
+		if( args.length != 3 ) {
+			System.out.println("usage: java FTTCPClient <host> <port> <filename>") ;
+			System.exit(0) ;
+		}
+		// Implement here the required code for the client ...
+			
+	}
+}
